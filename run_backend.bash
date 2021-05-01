37c1dcb74d57894f43d8452c4ab30d9309ce5533
Aster Bodden <asterb96@gmail.com>
2021-04-30 18:02:18 -0600

    uncomment section

--- run_backend.bash
+++ run_backend.bash
@@ -1,12 +1,12 @@
-#!/bin/bash 
-# user="boddenme"
-# password="1T2O3D4O"
-# database="todolist"
+!/bin/bash 
+user="boddenme"
+password="1T2O3D4O"
+database="todolist"
 
-# echo "Creating Database"
-# mysql --execute="CREATE DATABASE $database"
-# mysql --execute="create user '$user'@'%' identified by '$password'"
-# mysql --execute="grant all on $database.* to '$user'@'%'"
+echo "Creating Database"
+mysql --execute="CREATE DATABASE $database"
+mysql --execute="create user '$user'@'%' identified by '$password'"
+mysql --execute="grant all on $database.* to '$user'@'%'"
 
 echo "starting  Backend"
 cd todolist_be
